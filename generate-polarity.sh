70252bff3da324dc98f72dc0b8fad2ac321141c1
Your Name <gim.kobayashi@gmail.com>
2017-04-17 12:01:36 +0900

    i

--- generate-polarity.sh
+++ generate-polarity.sh
@@ -1,3 +1,7 @@
+if [ ! -e dataset/rakuten_reviews_wakati.txt ]; then
+wget https://www.dropbox.com/s/jsnzwdtskq1zgrh/rakuten_reviews_wakati.txt
+mv rakuten_reviews_wakati.txt dataset/ 
+fi
 kotlin ./bin/predealingTools.jar  >  ./dataset/term_index.txt
 kotlin ./bin/predealingTools.jar --svmIndexer > ./dataset/svm.fmt
 ./bin/train -s 0 ./dataset/svm.fmt
